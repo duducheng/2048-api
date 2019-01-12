31f06e34639c51faf188d3a112fd86303130d01c
lvoleing <43130710+lvoleing@users.noreply.github.com>
2019-01-12 20:18:50 +0800

    Add files via upload

--- game2048/agents.py
+++ game2048/agents.py
@@ -1,13 +1,15 @@
-import numpy as np
-
-
+﻿import numpy as np
+import torch
+from torchvision import transforms
+import os
+from Net import myNet
+from Net import testNet
 class Agent:
     '''Agent Base.'''
 
     def __init__(self, game, display=None):
         self.game = game
         self.display = display
-
     def play(self, max_iter=np.inf, verbose=False):
         n_iter = 0
         while (n_iter < max_iter) and (not self.game.end):
@@ -25,7 +27,35 @@ class Agent:
         direction = int(input("0: left, 1: down, 2: right, 3: up = ")) % 4
         return direction
 
+class MyAgent(Agent):
 
+    def __init__(self, game, display=None):
+        if game.size != 4:
+            raise ValueError("`%s` can only work with game of `size` 4." % self.__class__.__name__)
+        super().__init__(game, display)
+        modelPath = os.getcwd() + '/model/model.pth'
+        model=myNet()
+        model.load_state_dict(torch.load(modelPath))
+        self.search_func = model
+
+    def pre_process(self):
+        board = self.game.board
+        board[board==0] = 1
+        board = np.log2(board).flatten()
+        board = board.reshape((4, 4))
+        board = board[:, :, np.newaxis]
+        board = transforms.ToTensor()(board)
+        board = torch.unsqueeze(board, dim=0).float()
+        return board
+
+    def step(self):
+        input = self.pre_process()
+        output = self.search_func(input)
+        max_idx = np.where(output==torch.max(output))[1]
+        counts = np.bincount(max_idx)
+        direction = np.argmax(counts)
+        return direction
+        
 class RandomAgent(Agent):
 
     def step(self):
